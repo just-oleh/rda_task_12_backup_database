0478b7d8b5b4b60297d573b9ded45e4ef318e9a3
Oleh Musurivskyi <musurivskiioleg@gmail.com>
2026-04-24 01:41:26 +0300

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB | mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info --insert-ignore ShopDB | mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment
